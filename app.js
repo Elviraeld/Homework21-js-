dbfb4ad7db0137ea6f6c0c6877d972be2b64f783
Elviraeld <elyaali05@gmail.com>
2024-09-03 18:08:13 +0400

    task1

---
+++ app.js
@@ -0,0 +1,8 @@
+let dollar = prompt("Hesabinizdaki dollar sayini qeyd edin");
+let rubl = prompt("Hesabinizdaki rubl sayini qeyd edin");
+let newOne = dollar * 75;
+
+function myFunc(newOne,rubl){
+    console.log(+newOne + +rubl);
+}
+ myFunc(newOne,rubl);
